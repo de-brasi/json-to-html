89c4150e7e681a82e25d48a5879278a84969ceb2
de_brasi <ya.kakadu.ru@yandex.ru>
2023-05-01 21:31:06 +0300

    классы с типами теперь унаследованы

--- markdown_converter.py
+++ markdown_converter.py
@@ -5,18 +5,12 @@ from dataclasses import dataclass
 
 
 @dataclass
-class FieldTypes:
+class Types:
     """
-    Класс описывающий всевозможные ключи словарей-записей исходного json-документа.
-
-    Внимание! Если класс будет расширен какими-то функциями,
-    либо членами, не являющимися ключом словаря-записи,
-    необходимо при названии их дописывать префикс additional!
+    Only for inheritance.
+    Allows you to create classes similar to a 'dataclass' and
+    get aggregated information about class members
     """
-
-    # Types enumeration
-    type = "type"
-    content = "content"
 
     # Additional members enumeration
     additional_list_of_types = []
@@ -48,8 +42,19 @@ class FieldTypes:
         return self.additional_list_of_types
 
 
-@dataclass
-class ContentTypes:
+class FieldTypes(Types):
+    """
+    Класс описывающий всевозможные ключи словарей-записей исходного json-документа.
+
+    Внимание! Если класс будет расширен какими-то функциями,
+    либо членами, не являющимися ключом словаря-записи,
+    необходимо при названии их дописывать префикс additional!
+    """
+    type = "type"
+    content = "content"
+
+
+class ContentTypes(Types):
     """
     Класс описывающий всевозможные типы записей из исходного json-документа.
 
@@ -57,41 +62,10 @@ class ContentTypes:
     либо членами, не являющимися ключом словаря-записи,
     необходимо при названии их дописывать префикс additional!
     """
-
-    # Types enumeration
     text = 'Text'
     title = 'Title'
     list = 'List'
     image = 'image'
-
-    # Additional members enumeration
-    additional_list_of_types = []
-
-    def additional_function_count_members(self) -> int:
-        """
-        Return read-only list of values contained by types members
-        """
-
-        # Its count all public names, except extra names
-        #   (functions, values that are not dict keys etc.)
-        return len(self.additional_function_get_members())
-
-    def additional_function_get_members(self) -> list:
-        """
-        Return read-only list
-        """
-
-        if not self.additional_list_of_types:
-            # Its collect all public names, except extra names
-            #   (functions, values that are not dict keys etc.)
-            custom_prefix = "additional"
-            self.additional_list_of_types = [
-                self.__class__.__dict__[attr] for attr in self.__dir__() if (
-                        not attr.startswith('__') and
-                        not attr.startswith(custom_prefix)
-                )
-            ]
-        return self.additional_list_of_types
 
 
 class ValidationException(Exception):
