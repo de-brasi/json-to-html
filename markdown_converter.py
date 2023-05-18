d1a542a81cca4066a9bc15261a41516192e2214c
de_brasi <ya.kakadu.ru@yandex.ru>
2023-05-18 23:25:07 +0300

    with converting to html

--- markdown_converter.py
+++ markdown_converter.py
@@ -1,4 +1,7 @@
 import json
+import re
+import markdown
+
 from typing import Dict
 from dataclasses import dataclass
 
@@ -55,26 +58,24 @@ class TypesEnum:
 
 
 class FieldTypesEnum(TypesEnum):
-    # TODO: translate
     """
-    Класс описывающий всевозможные ключи словарей-записей исходного json-документа.
+    A class that describes all possible keys of the dictionaries-records of the original json document.
 
-    Внимание! Если класс будет расширен какими-то функциями,
-    либо членами, не являющимися ключом словаря-записи,
-    необходимо при названии их дописывать префикс additional!
+    Attention! If the class is extended with some functions,
+    or members that are not a dictionary entry key,
+    it is necessary to add the prefix "additional" when naming them!
     """
     type = "type"
     content = "content"
 
 
 class ContentTypesEnum(TypesEnum):
-    # TODO: translate
     """
-    Класс описывающий всевозможные типы записей из исходного json-документа.
+    A class that describes all kinds of records from the original json document.
 
-    Внимание! Если класс будет расширен какими-то функциями,
-    либо членами, не являющимися ключом словаря-записи,
-    необходимо при названии их дописывать префикс additional!
+    Attention! If the class is extended with some functions,
+    or members that are not a dictionary entry key,
+    it is necessary to add the prefix additional when naming them!
     """
     text = 'Text'
     title = 'Title'
@@ -87,7 +88,6 @@ CONTENT_TYPES = ContentTypesEnum()
 
 
 def validate_content(to_validate: Dict) -> None:
-    # TODO: translate
     """
     Raises ValidationException error if to_validate item has unexpected structure,
     does nothing otherwise.
@@ -126,10 +126,17 @@ class Converter:
             CONTENT_TYPES.image: "![]({})",
         }
 
-        self.markdown_list_separator = '\n\n'
-        self.markdown_list_line_break = ' ' * 2     # double space in the EOL for line-break
+        self.md_list_separator = '\n\n'
+        self.md_list_line_break = ' ' * 2     # double space in the EOL for line-break
 
-        self.markdown_new_line = '\n\n'
+        self.md_new_line = '\n\n'
+
+        # Warning! Character '\\' may be incorrect interpret in non Unix-like systems.
+        # Attention! List of special characters:
+        #   ['#', '*', '-', '+', '\\', '_', '{', '}', '[', ']', '(', ')', '#', '.', '!']
+        # For some reason the local Markdown parser doesn't require escaping some characters.
+        # Maybe it depends on the version of the Markdown engine.
+        self.md_special_symbols = ['*', '-', '+']
 
     def convert(self, value_type: str, value: str) -> str:
         after_conversion = ""
@@ -144,37 +151,64 @@ class Converter:
                 after_conversion = self.markup_patterns[CONTENT_TYPES.text].format(value)
             case CONTENT_TYPES.list:
                 list_items = [value for value in
-                              value.split(self.markdown_list_separator) if value]
+                              value.split(self.md_list_separator) if value]
                 for i in range(len(list_items)):
                     one_list_item = list_items[i]
                     correct_version = self._correct_line(one_list_item)
-                    correct_version = correct_version.replace('\n', self.markdown_list_line_break + '\n')
+                    correct_version = correct_version.replace('\n', self.md_list_line_break + '\n')
                     list_items[i] = self.markup_patterns[CONTENT_TYPES.list].format(correct_version)
 
-                after_conversion = self.markdown_new_line.join(list_items)
-                after_conversion += self.markdown_new_line
+                after_conversion = self.md_new_line.join(list_items)
+                after_conversion += self.md_new_line
             case CONTENT_TYPES.image:
                 after_conversion = \
                     self.markup_patterns[CONTENT_TYPES.image].format(value)
 
-        after_conversion += self.markdown_new_line
+        after_conversion += self.md_new_line
         return after_conversion
 
     def _correct_line(self, source_line) -> str:
-        # todo: обрабатывать html-like стиль верстки (но там сложно, надо проверять много лексем)
+        # TODO: добавить варианты постобработки
+        #  (например оставить как есть или удалить символы, которые интерпретируются как специальные)
 
-        source_word_by_char = [x for x in source_line]
+        # Convert self.md_special_symbols to one string "...<symbols>..."
+        # Make with it md pattern "[...<symbols>...]" and remember detected occurrence with ()
 
-        # todo: построить автомат для простого случая лексем
+        # Every special symbol must be concatenated with '\' symbol for escaping
+        # Be careful with '\', '\\' and raw Python string in this code!
+        single_character_pattern = ''.join(['\\' + char for char in self.md_special_symbols])
+        assert single_character_pattern
 
-        # вроде так как строка неизменяема,
-        # то добавление 1 символа к строке создаст полную копию,
-        # то есть имеет смысл создать список с буквами и работать со списком
+        pattern = r"([{}])".format(single_character_pattern)
+        source_line = re.sub(r"([{}])".format(single_character_pattern), r"\\\1", source_line)
+
+        # TODO: 1) всякие случаи с 2ми символами типа "1.", и тд (в основном для листов)
+        #       2) всякие варианты с #
+        #       3) особые случаи со скобками (например ![Alt text](/path/to/img.jpg))
+        #       4) посмотреть где каждая скобка используется в особых случаях
+        #       Полный список
+        #           List of special characters: ['#', '*', '-', '+', '\\', '_',
+        #                                       '{', '}', '[', ']', '(', ')', '#', '.', '!']
+
+        # If there are sections similar to html code,
+        # which is specially processed by the Markdown parser
+        markdown_left_angle_bracket_escape_sequence = r"&lt;"
+        source_line = re.sub(r"\<", markdown_left_angle_bracket_escape_sequence, source_line)
 
         return source_line
 
 
-if __name__ == "__main__":
+def main(source, destination):
+    def get_id_with_increment() -> str:
+        nonlocal cur_header_id_num, header_id_prefix
+
+        res = header_id_prefix + str(cur_header_id_num)
+        cur_header_id_num += 1
+        return res
+
+    cur_header_id_num = 0
+    header_id_prefix = "header"
+
     converter = Converter()
     # todo: opened all files from directory or getting file name
     with open("./source_json/data.json", 'r') as src:
@@ -187,3 +221,20 @@ if __name__ == "__main__":
                     item[FIELD_TYPES.content])
                 dst.write(conversion_result)
 
+        # read from md
+        with open("./test.md", 'r') as f:
+            text = f.read()
+            html = markdown.markdown(text)
+
+        # write to html with indexes
+        with open('res.html', 'w') as f:
+            header_pattern = r"<h1>"
+            html = re.sub(header_pattern,
+                          lambda exp: r'<h1 id="{}">'.format(get_id_with_increment()),
+                          html)
+
+            f.write(html)
+
+
+if __name__ == "__main__":
+    main()
